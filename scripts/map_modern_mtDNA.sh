e02ab6ed4b3bba90c4dc8efe6a0754660b60642f
mmerkin <163770255+mmerkin@users.noreply.github.com>
2026-04-15 16:50:05 +0100

    Update map_modern_mtDNA.sh

--- scripts/map_modern_mtDNA.sh
+++ scripts/map_modern_mtDNA.sh
@@ -8,7 +8,7 @@ remove_temp=false
 set -euo pipefail
 
 if [[ $# -ne 3 ]]; then
-    echo "Usage: $0 reference.fa read_path output_path" >&2
+    echo "Usage: bash $0 reference.fa read_path output_path" >&2
     exit 1
 fi
 
@@ -17,45 +17,90 @@ read_path="$2"
 output_path="$3"
 
 # Create output directory
-mkdir -p "$output_path"
+mkdir -p "$output_path/bams"
 temp="${output_path}/temp"
 
 # Send errors to log file
 
-exec 3>&1 1>"map_modern_mtDNA_logfile.txt" 2>&1
-trap "echo 'ERROR: An error occurred during execution, check map_modern_mtDNA_logfile.txt for details.' >&3" ERR
-trap '{ set +x; } 2>/dev/null; echo -n "[$(date -Is)]  "; set -x' DEBUG
+logfile="$output_path/modern_mapping_log.txt"
+
+exec 3>&1
+exec >"$logfile" 2>&1
+
+missing=()
+
+for cmd in bwa-mem2 bam; do
+    if ! command -v "$cmd" >/dev/null 2>&1; then
+        missing+=("$cmd")
+    fi
+done
+
+if ((${#missing[@]} > 0)); then
+    echo "There are missing commands. Have you activated the conda environment?" >&3
+    exit 1
+fi
+
+trap 'echo "ERROR: An error occurred during execution, check '"$logfile"' for details." >&3' ERR
+export PS4='[$(date -Is)] + '
+trap 'set -x' DEBUG
+
+# Command to output to terminal only
+
+terminal() {
+    echo "[$(date -Is)] $*" >&3
+}
+
+# Command to output to both terminal and log
+
+all() {
+    "$@" 2>&1 | tee /dev/fd/3
+}
+
+
+# Clear screen
+
+printf '\n%.0s' {1..50} >&3
+all echo -e "Mapping and filtering reads\n\n"
+
 
 # Process the bam files
+
 
 for file in $read_path/*R1*; do
 
 # Set variables for next task
-sample_name="${f%%R1*}"
+
+sample_name="${file##*/}"
+sample_name="${sample_name%%R1*}"
 sample_name="${sample_name%[_\.]}"
-filepath="${read_path}/${sample_name}"
-echo -e "\nMoving to sample $sample_name" | tee /dev/fd/3 # tee displays the echo output in the terminal now that stdout is sent to a log file
-mkdir -p "$temp/$filetag"
-mkdir -p "$output_path/$filetag"
+all echo -e "\nMoving to sample $sample_name"
+mkdir -p "$temp/$sample_name"
 
 R1="$file"
 R2="${file/R1/R2}"
 RG="@RG\tID:${sample_name}\tSM:${sample_name}\tPL:ILLUMINA"
-bwa-mem2 mem -t $threads-R "$RG" $reference "$R1" "$R2" | \
-samtools view -@ $threads -b -F 4 -q 20 -o | \
-samtools sort -n -@ $threads -o "$temp/${sample_name}.sort.n.bam"
+bwa-mem2 mem -t $threads -R "$RG" $reference "$R1" "$R2" | \
+samtools view -@ $threads -b -F 4 -q 20 | \
+samtools sort -n -@ $threads -o "$temp/$sample_name/${sample_name}.sort.n.bam"
 
-samtools fixmate -m -@ $threads "$temp/${sample_name}.sort.n.bam" "$temp/${sample_name}.fixmate.bam"
+all echo -e "Mapped reads"
 
-samtools sort -@ $threads -o "$temp/${sample_name}.sort.p.bam" "$temp/${sample_name}.fixmate.bam"
 
-samtools markdup -r -@ $threads "$temp/${sample_name}.sort.p.bam" "$temp/${sample_name}.dedup.bam"
+samtools fixmate -m -@ $threads "$temp/$sample_name/${sample_name}.sort.n.bam" "$temp/$sample_name/${sample_name}.fixmate.bam"
 
-samtools index "$temp/${sample_name}.dedup.bam"
+samtools sort -@ $threads -o "$temp/$sample_name/${sample_name}.sort.p.bam" "$temp/$sample_name/${sample_name}.fixmate.bam"
 
-bamUtil clipOverlap --in "$temp/$filetag/$filetag.rmdup.bam" --out "$temp/$filetag/$filetag.noclip.bam"
+samtools markdup -r -@ $threads "$temp/$sample_name/${sample_name}.sort.p.bam" "$temp/$sample_name/${sample_name}.dedup.bam"
 
-samtools index "$temp/$filetag/$filetag.noclip.bam"
+samtools index "$temp/$sample_name/${sample_name}.dedup.bam"
+
+all echo -e "Removed PCR duplicates"
+
+bam clipOverlap --in "$temp/$sample_name/${sample_name}.dedup.bam" --out "$output_path/bams/${sample_name}.bam"
+
+samtools index "$output_path/bams/${sample_name}.bam"
+
+all echo -e "Clipped overlaps\n"
 
 if $remove_temp; then
 rm -r $temp
@@ -63,4 +108,4 @@ fi
 
 done
 
-echo -e "\nAll samples processed!" | tee /dev/fd/3
+all echo -e "\nAll samples processed!"
